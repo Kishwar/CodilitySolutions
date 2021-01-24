a02575f4d250f7cc7e5593137ce4a7646b9249bd
Kishwar Kumar <kumar.kishwar@gmail.com>
2021-01-24 09:50:05 +0100

    updates

--- codilitySolutions/codilitySolutions/BinaryGap.cpp
+++ codilitySolutions/codilitySolutions/BinaryGap.cpp
@@ -7,9 +7,15 @@
 
  /*
   PROBLEM:
-    A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+    A binary gap within a positive integer N is
+    any maximal sequence of consecutive zeros that
+    is surrounded by ones at both ends in the binary
+    representation of N.
   
-  For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+    For example, given N = 1041 the function should
+    return 5, because N has binary representation
+    10000010001 and so its longest binary gap is of
+    length 5.
   */
 
 
